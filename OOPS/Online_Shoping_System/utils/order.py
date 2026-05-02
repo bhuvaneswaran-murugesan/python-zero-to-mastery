f8a02ce660a3304675a90b62fae5d4d021cafd17
BhuvaneswaranMurugesan <bhuvaneswaran.kanini@gmail.com>
2026-05-02 22:23:56 +0530

    [OSS] - Updated order and main methods

--- OOPS/Online_Shoping_System/utils/order.py
+++ OOPS/Online_Shoping_System/utils/order.py
@@ -6,9 +6,9 @@ class Order():
     def __init__(self, user, cart):
         self.__user = user
         self.__cart = cart
+        Order.order_id += 1
     
     def getorder_info(self):
-        Order.order_id += 1
         user_name,_ = self.__user.get_info()
         
         return f"Order ID: {Order.order_id}\nUser: {user_name}\nTotal Amount: {self.__cart.get_total_amount()}"
